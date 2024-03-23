4361b252bab4225cc08087e8c910b90e1fa73bbc
jonav01 <jonescomputer443@gmail.com>
2024-03-23 17:20:29 +0530

    feat/setup comms between multiple microservices

--- backend/apps/tasks/src/tasks.module.ts
+++ backend/apps/tasks/src/tasks.module.ts
@@ -3,11 +3,12 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { join } from 'path';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
 import { validateEnv } from './validation';
 import { TasksService } from './tasks/tasks.service';
 import { LibsModule } from 'libs/common/src/libs.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -25,6 +26,26 @@ import { LibsModule } from 'libs/common/src/libs.module';
       envFilePath: ['.env.development.local', '.env.production'],
       validationSchema: Joi.object(validateEnv),
     }),
+    ClientsModule.registerAsync([
+      {
+        name: 'TASKS_SERVICE',
+        useFactory: async (configService: ConfigService) => {
+          return {
+            transport: Transport.KAFKA,
+            options: {
+              client: {
+                clientId: 'tasks',
+                brokers: [configService.get<string>('KAFKA_BROKER_URL')],
+              },
+              consumer: {
+                groupId: 'scheduler-microservice',
+              },
+            },
+          };
+        },
+        inject: [ConfigService],
+      },
+    ]),
     LibsModule,
   ],
   controllers: [],
--- backend/apps/tasks/src/tasks/tasks.service.ts
+++ backend/apps/tasks/src/tasks/tasks.service.ts
@@ -1,14 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { Prisma } from 'prisma-client-tasksDB';
 import { PrismaService } from 'libs/common/src/dbConnection.service';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
 export class TasksService {
-  constructor(private readonly prisma: PrismaService) {}
+  private readonly logger = new Logger(TasksService.name);
+  constructor(
+    private readonly prisma: PrismaService,
+    @Inject('TASKS_SERVICE') private readonly tasksClient: ClientKafka,
+  ) {}
 
   async createTasks(data: Prisma.tasksUncheckedCreateInput) {
-    return await this.prisma.tasks.create({
-      data,
-    });
+    try {
+      const createdTask = await this.prisma.tasks.create({
+        data,
+      });
+      this.tasksClient.emit('tasks-created', createdTask);
+      this.logger.log('Tasks created event successfull');
+      return createdTask;
+    } catch (error) {
+      this.logger.error('Error occured while creating tasks');
+      throw new InternalServerErrorException(
+        'Some error occured while creating task',
+        error,
+      );
+    }
   }
 }
