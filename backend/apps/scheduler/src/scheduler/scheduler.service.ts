9361e725c1e51c991752581153aac021d22205bd
jonav01 <jonescomputer443@gmail.com>
2024-04-06 20:52:32 +0530

    feat/cronjob updating db fulfilled

--- backend/apps/scheduler/src/scheduler/scheduler.service.ts
+++ backend/apps/scheduler/src/scheduler/scheduler.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { SchedulerRegistry } from '@nestjs/schedule';
 import { CronJob } from 'cron';
-import { Prisma } from 'prisma-client-tasksDB';
+import { Prisma, $Enums } from 'prisma-client-tasksDB';
 import { PrismaService } from 'libs/common/src';
 
 interface ICreatedTask {
@@ -26,7 +26,6 @@ interface ICreatedJob {
 @Injectable()
 export class SchedulerService {
   private readonly logger = new Logger(SchedulerService.name);
-  private timeExpression: string;
   constructor(
     private readonly schedulerRegistry: SchedulerRegistry,
     private readonly prisma: PrismaService,
@@ -39,47 +38,51 @@ export class SchedulerService {
     const scheduledJob: ICreatedJob = await this.upsertScheduler(
       data.id,
       {
+        time: cronDateExpression,
+        status: 'Active',
+      },
+      {
         taskId: data.id,
         taskName: `dynamicJob-${data.type}-${data.id}`,
         time: cronDateExpression,
         status: 'Active',
       },
-      {
-        time: cronDateExpression,
-        status: 'Active',
-      },
     );
 
-    this.addCronJob(scheduledJob.taskName, cronDateExpression);
+    this.addCronJob(
+      scheduledJob.taskName,
+      cronDateExpression,
+      scheduledJob.taskId,
+    );
     this.getCrons();
   }
 
   getCrons() {
     const jobs = this.schedulerRegistry.getCronJobs();
-    jobs.forEach((value, key, map) => {
+
+    jobs.forEach((value, key) => {
       let next;
       try {
         next = value.nextDate().toUTC();
       } catch (e) {
         next = 'error: next fire date is in the past!';
       }
+      this.logger.log(`realDate: ${value.cronTime.sendAt()} -> next: ${next}`);
       this.logger.log(`job: ${key} -> next: ${next}`);
     });
   }
 
-  addCronJob(name: string, cronTime: string) {
-    const job = new CronJob(
-      cronTime,
-      () => {
-        this.logger.warn(`time (${cronTime}) for job ${name} to run!`);
-        job.stop();
+  addCronJob(name: string, cronTime: string, taskId: string) {
+    const job = CronJob.from({
+      cronTime: cronTime,
+      onTick: async () => {
+        // update the task status
+        await this.updateScheduler(taskId, {
+          status: $Enums.TaskStatus.Finished,
+        });
+        this.logger.log('Job Executed');
       },
-
-      () => {
-        this.logger.log('Job finished');
-      },
-      false,
-    );
+    });
 
     this.schedulerRegistry.addCronJob(name, job);
     job.start();
@@ -89,8 +92,8 @@ export class SchedulerService {
 
   async upsertScheduler(
     taskId: string,
-    createObj: Prisma.schedulerUncheckedCreateInput,
     updateObj: Prisma.schedulerUncheckedUpdateInput,
+    createObj: Prisma.schedulerUncheckedCreateInput,
   ) {
     return await this.prisma.scheduler.upsert({
       where: {
@@ -101,13 +104,25 @@ export class SchedulerService {
     });
   }
 
+  async updateScheduler(
+    taskId: string,
+    updateObj: Prisma.schedulerUncheckedUpdateInput,
+  ) {
+    return await this.prisma.scheduler.update({
+      where: {
+        taskId,
+      },
+      data: updateObj,
+    });
+  }
+
   convertUtcToCronExpression(dateString: string) {
     const date = new Date(dateString);
     const seconds = date.getSeconds();
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const dayOfMonth = date.getDate();
-    const month = date.getMonth();
+    const month = date.getMonth() + 1;
     const dayOfWeek = date.getDay();
 
     return `${seconds || '*'} ${minutes || '*'} ${hours || '*'} ${dayOfMonth || '*'} ${month || '*'} ${dayOfWeek || '*'}`;
